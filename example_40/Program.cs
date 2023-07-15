73aab302cbce2eda53f7d22803957b3c2ca332fd
Anton <razjivin.anton@yandex.ru>
2023-07-15 12:34:18 +0300

    add example_40

---
+++ example_40/Program.cs
@@ -0,0 +1,22 @@
+﻿/* Задача 39: Напишите программу, которая принимает
+на вход три чила и проверяет, может ли существовать
+треугольник со сторонами такой длины.
+Теорема о неравенстве треуголника: каждая сторона
+треуголтника меньше суммы двух других сторон
+*/
+
+Console.Write("Введи длину 1 стороны: ");
+int side1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введи длину 2 стороны: ");
+int side2 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введи длину 3 стороны: ");
+int side3 = Convert.ToInt32(Console.ReadLine());
+
+bool existsTriangle = IsExistsTriangle(side1, side2, side3);
+Console.Write(existsTriangle ? "Треугольник существует" : "Треугольник не существует");
+
+bool IsExistsTriangle(int a, int b, int c) {
+    return a < b + c && b < a + c && c < b + a;
+}
